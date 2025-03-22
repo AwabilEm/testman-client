03c2f13296b281dc8b4f2a52243b73102a6cd973
wabilEm <awabil.pilgrim@gmail.com>
2025-03-22 12:21:45 +0000

    test6

--- tests/newonboarding2.spec.ts
+++ tests/newonboarding2.spec.ts
@@ -7,8 +7,8 @@ import { checkHomeStudio } from '../util/homeStudioIsOnboarded.ts';
 
 
 
-const email = 'awabil.test5@gmail.com';
-const PhoneNumber = '810-400-0007';
+const email = 'awabil.test6@gmail.com';
+const PhoneNumber = '810-400-0008';
 
 const password = 'TestUser@1'
 const fName = 'test';
